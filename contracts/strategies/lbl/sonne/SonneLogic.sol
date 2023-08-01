4babd28f61c5432ccbc36f1597076dae7d2a9db0
ra0321 <alphanetra0321@gmail.com>
2023-08-01 19:59:26 +0200

    add sonne logic

---
+++ contracts/strategies/lbl/sonne/SonneLogic.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.13;
+pragma abicoder v2;
+
+import "../../../LendingLogic.sol";
+import "../../../Interfaces/ICompound.sol";
+
+contract SonneLogic is LendingLogic {
+    /*** Override internal function ***/
+}
