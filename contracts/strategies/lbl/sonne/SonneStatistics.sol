c0c14678051af6d6fa880bb74efed5c3f5c2f14c
ra0321 <alphanetra0321@gmail.com>
2023-08-02 19:59:51 +0200

    add sonne statistics

---
+++ contracts/strategies/lbl/sonne/SonneStatistics.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.13;
+pragma abicoder v2;
+
+import "../../../StatisticsBase.sol";
+import "../../../Interfaces/ICompound.sol";
+
+contract SonneStatistics is StatisticsBase {}
