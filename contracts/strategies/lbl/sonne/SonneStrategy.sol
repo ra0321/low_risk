76b99cd3f0c34ee90c1041cb840ba59f679ad135
ra0321 <alphanetra0321@gmail.com>
2023-08-03 20:02:22 +0200

    add sonne strategy

---
+++ contracts/strategies/lbl/sonne/SonneStrategy.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.13;
+pragma abicoder v2;
+
+import "./../../../Interfaces/ICompound.sol";
+import "./../LendBorrowLendStrategy.sol";
+
+contract SonneStrategy is LendBorrowLendStrategy {
+    /*** Override Internal Functions ***/
+}
