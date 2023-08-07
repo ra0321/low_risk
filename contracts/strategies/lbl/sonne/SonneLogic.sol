fa7d36593f0467cfcfd7bd9480e2f904b9b10c94
ra0321 <alphanetra0321@gmail.com>
2023-08-07 08:06:28 +0200

    add Sonne logic contract

--- contracts/strategies/lbl/sonne/SonneLogic.sol
+++ contracts/strategies/lbl/sonne/SonneLogic.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.13;
 pragma abicoder v2;
 
 import "../../../LendingLogic.sol";
-import "../../../Interfaces/ICompound.sol";
+import "../../../Interfaces/ISonne.sol";
 
 contract SonneLogic is LendingLogic {
     /*** Override internal function ***/
