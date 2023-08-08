5d699945a6b5ee6bfbce55860707df3527da12ac
ra0321 <alphanetra0321@gmail.com>
2023-08-08 12:33:33 +0200

    update SonneStatistics contract

--- contracts/strategies/lbl/sonne/SonneStatistics.sol
+++ contracts/strategies/lbl/sonne/SonneStatistics.sol
@@ -4,6 +4,6 @@ pragma solidity ^0.8.13;
 pragma abicoder v2;
 
 import "../../../StatisticsBase.sol";
-import "../../../Interfaces/ICompound.sol";
+import "../../../Interfaces/ISonne.sol";
 
 contract SonneStatistics is StatisticsBase {}
